6b90c7802266621596ce624486c813aaf7e8540d
Chris Banks <chris.banks@digital.cabinet-office.gov.uk>
2022-03-18 14:45:20 +0000

    Shellcheck and style fixes for static-error-page-upload.sh.
    - Remove unnecessary line continuations and `&&`.
    - Use a plain string instead of an array; it's easier to read and more
      idiomatic in this case.
    - Use `curl -o` instead of redirecting the output.
    - Define the output directory once.
    - Don't fail if the output directory already exists.

--- charts/govuk-rails-app/static-error-page-upload.sh
+++ charts/govuk-rails-app/static-error-page-upload.sh
@@ -1,8 +1,13 @@
-set -eu && \
-export ERROR_PAGES=(400 401 403 404 405 406 410 422 429 500 502 503 504) && \
-mkdir /error_pages && \
-for page in "${ERROR_PAGES[@]}"; do \
+#!/bin/sh
+set -eu
+
+ERROR_PAGES="400 401 403 404 405 406 410 422 429 500 502 503 504"
+OUTPUT_PATH=/var/error_pages
+
+mkdir -p "${OUTPUT_PATH}"
+for page in $ERROR_PAGES; do
   echo "${page}"
-  curl --fail "http://static/templates/${page}.html.erb" > /error_pages/${page}.html; \
-done; \
-aws s3 sync "/error_pages/" "s3://govuk-app-assets-${GOVUK_ENVIRONMENT}/error_pages/"
+  curl --fail --output "${OUTPUT_PATH}/${page}.html" "http://static/templates/${page}.html.erb"
+done
+
+aws s3 sync "${OUTPUT_PATH}/" "s3://govuk-app-assets-${GOVUK_ENVIRONMENT}/error_pages/"
