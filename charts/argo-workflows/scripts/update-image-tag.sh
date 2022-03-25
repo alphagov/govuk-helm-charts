7987e85ae1cc3e01cfb71a53b570f71e28c884a5
Sean Rankine <sean.rankine@me.com>
2022-03-25 15:02:51 +0000

    Install gh-cli for the update-image-tag script
    This adds the commands to install the gh-cli (as per the docs here:
    https://github.com/cli/cli/blob/trunk/docs/install_linux.md#debian-ubuntu-linux-raspberry-pi-os-apt).
    Ideally this would be installed into the container image, instead of
    installing at script runtime.

--- charts/argo-workflows/scripts/update-image-tag.sh
+++ charts/argo-workflows/scripts/update-image-tag.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-apt-get update && apt-get install -y git
+
+apt-get update && apt-get install -y git curl
+
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+
+apt-get update && apt-get install -y gh
 
 BRANCH="update-image-tag/${APPLICATION}/${ENVIRONMENT}/${IMAGE_TAG}"
 FILE="charts/argocd-apps/image-tags/${ENVIRONMENT}/${APPLICATION}"
