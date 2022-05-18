a187acbe4bffcdfa9807f1b54d874369cd6fae67
Chris Banks <chris.banks@digital.cabinet-office.gov.uk>
2022-05-18 16:50:21 +0100

    Make it easier to debug the error-page-upload job.
    Allow enabling the job by setting a Helm value. This allows running the
    job when deploying Static under a different name.
    
    Also pass the service name for Static into the script so that it uses
    the right one when testing (again, when the chart is deployed under a
    different name).
    
    Enable tracing in the script so that it's clearer from the logs as to
    what's running when.
    
    Add a description to the Job object.

--- charts/govuk-rails-app/upload-static-error-pages.sh
+++ charts/govuk-rails-app/upload-static-error-pages.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-set -eu
+set -eux
 
 ERROR_PAGES="400 401 403 404 405 406 410 422 429 500 502 503 504"
 OUTPUT_PATH=/var/error_pages
@@ -7,7 +7,7 @@ OUTPUT_PATH=/var/error_pages
 mkdir -p "${OUTPUT_PATH}"
 for page in $ERROR_PAGES; do
   echo "${page}"
-  curl --fail --output "${OUTPUT_PATH}/${page}.html" "http://static/templates/${page}.html.erb"
+  curl --fail --output "${OUTPUT_PATH}/${page}.html" "http://${SERVICE}/templates/${page}.html.erb"
 done
 
 aws s3 sync "${OUTPUT_PATH}/" "s3://govuk-app-assets-${GOVUK_ENVIRONMENT}/error_pages/"
