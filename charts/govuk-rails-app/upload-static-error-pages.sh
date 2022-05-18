187e16dbc3a839eb90256fa86faa510548fc9760
Chris Banks <chris.banks@digital.cabinet-office.gov.uk>
2022-05-18 16:55:06 +0100

    Run static-error-page-upload job as non-root.
    This makes the job able to run again since we no longer allow root
    containers in the default and "apps" namespaces.

--- charts/govuk-rails-app/upload-static-error-pages.sh
+++ charts/govuk-rails-app/upload-static-error-pages.sh
@@ -2,7 +2,7 @@
 set -eux
 
 ERROR_PAGES="400 401 403 404 405 406 410 422 429 500 502 503 504"
-OUTPUT_PATH=/var/error_pages
+OUTPUT_PATH=/tmp
 
 mkdir -p "${OUTPUT_PATH}"
 for page in $ERROR_PAGES; do
