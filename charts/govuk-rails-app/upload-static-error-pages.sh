e3b7f90993c472704f423dcd814c406c83cf4162
Chris Banks <chris.banks@digital.cabinet-office.gov.uk>
2022-05-18 17:00:14 +0100

    Reuse the same connection when scraping error pages from Static.
    It seems `curl -f` bases its exit code on the status of the last
    request, even with `--fail-early`, hence the additional check to make
    sure we really downloaded all the files. I think the additional check is
    worth having anyway, especially since we've already had one bug in that
    area.
    
    See the [curl manpage] for explanation of how the substitution in `-o`
    works.
    
    [curl manpage]: https://curl.se/docs/manpage.html
    
    Tested: checked the failure and success cases manually; output looks
    good and it's easy to see what went wrong when it fails.

--- charts/govuk-rails-app/upload-static-error-pages.sh
+++ charts/govuk-rails-app/upload-static-error-pages.sh
@@ -1,13 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 set -eux
 
-ERROR_PAGES="400 401 403 404 405 406 410 422 429 500 502 503 504"
-OUTPUT_PATH=/tmp
+ERROR_PAGES='400 401 403 404 405 406 410 422 429 500 502 503 504'
+ERROR_PAGES_COMMA_SEPARATED=${ERROR_PAGES// /,}
+OUTPUT_PATH=/tmp/output
 
 mkdir -p "${OUTPUT_PATH}"
-for page in $ERROR_PAGES; do
-  echo "${page}"
-  curl --fail --output "${OUTPUT_PATH}/${page}.html" "http://${SERVICE}/templates/${page}.html.erb"
-done
-
-aws s3 sync "${OUTPUT_PATH}/" "s3://govuk-app-assets-${GOVUK_ENVIRONMENT}/error_pages/"
+cd "${OUTPUT_PATH}"
+curl --fail-early -fo '#1.html' "${SERVICE}/templates/{${ERROR_PAGES_COMMA_SEPARATED}}.html.erb"
+eval ls "{$ERROR_PAGES_COMMA_SEPARATED}.html" || (echo Failed to download one or more files.; exit 1)
+aws s3 sync . "s3://govuk-app-assets-${GOVUK_ENVIRONMENT}/error_pages/"
