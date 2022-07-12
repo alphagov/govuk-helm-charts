59d1c479685b1b832fc38afa52f6f6ef1415dc04
fredericfran-gds <frederic.francois@digital.cabinet-office.gov.uk>
2022-07-12 15:37:14 +0100

    Check for automatic_deploy_enabled before changing image_tag
    If automatic_deploy_enabled is false for given app, we do not update
    the image for automated deploys
    
    For manual deploys, image tag is always updated.
    
    Ref:
    1. [trello card](https://trello.com/c/5uj2U5rS/900-prevent-manual-deploys-being-overridden-by-cd)

--- charts/argo-workflows/scripts/update-image-tag.sh
+++ charts/argo-workflows/scripts/update-image-tag.sh
@@ -5,19 +5,7 @@ BRANCH="update-image-tag/${REPO_NAME}/${ENVIRONMENT}/${IMAGE_TAG}"
 FILE="charts/argocd-apps/image-tags/${ENVIRONMENT}/${REPO_NAME}"
 LATEST_GIT_SHA=$(git ls-remote "https://github.com/alphagov/${REPO_NAME}" HEAD | cut -f 1)
 
-git config --global user.email "${GIT_NAME}@digital.cabinet-office.gov.uk"
-git config --global user.name "${GIT_NAME}"
-
-gh auth setup-git
-gh repo clone alphagov/govuk-helm-charts -- --depth 1 --branch main
-
-cd "govuk-helm-charts" || exit 1
-
-# Exit successfully if image tag already set
-if [[ $(<"${FILE}") = "${IMAGE_TAG}" ]]; then
-  echo "Image tag already set as ${IMAGE_TAG}"
-# Relies on the assumption the IMAGE_TAG is a commit SHA
-elif [[ "${LATEST_GIT_SHA}" = "${IMAGE_TAG}" ]] || [[ "${MANUAL_DEPLOY}" = true ]]; then
+change_image_tag() {
   git checkout -b "${BRANCH}"
 
   yq -i '.image_tag = env(IMAGE_TAG)' "${FILE}"
@@ -29,7 +17,36 @@ elif [[ "${LATEST_GIT_SHA}" = "${IMAGE_TAG}" ]] || [[ "${MANUAL_DEPLOY}" = true 
   gh api repos/alphagov/govuk-helm-charts/merges -f head="${BRANCH}" -f base=main
   git push origin --delete "${BRANCH}"
 
-  echo "Done!"
+  echo "Pushed image_tag change to GitHub"
+}
+
+git config --global user.email "${GIT_NAME}@digital.cabinet-office.gov.uk"
+git config --global user.name "${GIT_NAME}"
+
+gh auth setup-git
+gh repo clone alphagov/govuk-helm-charts -- --depth 1 --branch main
+
+cd "govuk-helm-charts" || exit 1
+
+# Exit successfully if image tag already set
+current_image_tag="$(yq '.image_tag' "${FILE}")"
+if [[ "${current_image_tag}" = "${IMAGE_TAG}" ]]; then
+  echo "Image tag already set as ${IMAGE_TAG}"
+
+elif [[ "${MANUAL_DEPLOY}" = true ]]; then
+  change_image_tag
+
+# If automatic deploys, check automatic_deploys_enabled before proceeding.
+# Relies on the assumption the IMAGE_TAG is a commit SHA
+elif [[ "${LATEST_GIT_SHA}" = "${IMAGE_TAG}" ]]; then
+  auto_deploys="$(yq '.automatic_deploys_enabled' "${FILE}")"
+  # Auto deploys are enabled unless explicitly set to "false" (case insensitive).
+  if [[ "${auto_deploys,,}" != "false" ]]; then
+    change_image_tag
+  else
+    echo "Did not update image tag because automatic_deploys_enabled is set to false for app"
+  fi
+
 else
   echo "Image tag not updated for ${ENVIRONMENT}: image tag not the latest commit on main."
 fi
