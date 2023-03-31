48e98ab8dee6ca4a923c82bfe26dc1f399b6bf97
Sean Rankine <sean.rankine@me.com>
2023-03-31 16:56:19 +0100

    Do all checks for promotion in post-sync script
    Makes the post-sync "check-for-promotion" check if a particular
    deployment should be promoted and if automatic deployments has been
    enabled for an application. This also moves the script into a seperate
    file for legibility.

---
+++ charts/argo-services/scripts/check-for-promotion.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# Fetch the YAML configuration
+CONFIG_URL="https://raw.githubusercontent.com/alphagov/govuk-helm-charts/main/charts/app-config/image-tags/${ENVIRONMENT}/${REPO_NAME}"
+CONFIG_CONTENT=$(curl -Ls "${CONFIG_URL}")
+
+# Extract the values of automatic_deploys_enabled and promote_deployment
+automatic_deploys_enabled=$(echo "${CONFIG_CONTENT}" | yq '.automatic_deploys_enabled' -)
+promote_deployment=$(echo "${CONFIG_CONTENT}" | yq '.promote_deployment' -)
+
+# Check if both values are true
+if [ "${automatic_deploys_enabled,,}" == "true" ] && [ "${promote_deployment,,}" == "true" ]; then
+  echo "true"
+else
+  echo "false"
+fi
