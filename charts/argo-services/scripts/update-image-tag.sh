23bd04aed6e6e9e5e1847728f91cba95234ef314
Sean Rankine <sean.rankine@me.com>
2023-04-11 14:49:13 +0100

    Simplify update image tag script
    As the promotion logic had been removed, there is no need to have a
    function to encapsulate changing the image tag (we only call it once).
    This make the script easier to read.

--- charts/argo-services/scripts/update-image-tag.sh
+++ charts/argo-services/scripts/update-image-tag.sh
@@ -3,19 +3,6 @@ set -euo pipefail
 
 BRANCH="update-image-tag/${REPO_NAME}/${ENVIRONMENT}/${IMAGE_TAG}"
 FILE="charts/app-config/image-tags/${ENVIRONMENT}/${REPO_NAME}"
-CHANGED=false
-
-change_image_tag() {
-  git checkout -b "${BRANCH}"
-
-  yq -i '.image_tag = env(IMAGE_TAG)' "${FILE}"
-  yq -i '.promote_deployment = env(PROMOTE_DEPLOYMENT)' "${FILE}"
-
-  git add "${FILE}"
-  git commit -m "Update ${REPO_NAME} image tag to ${IMAGE_TAG} for ${ENVIRONMENT}"
-
-  CHANGED=true
-}
 
 git config --global user.email "${GIT_NAME}@digital.cabinet-office.gov.uk"
 git config --global user.name "${GIT_NAME}"
@@ -30,14 +17,19 @@ current_image_tag="$(yq '.image_tag' "${FILE}")"
 # Ignore if image tag already set
 if [[ "${current_image_tag}" = "${IMAGE_TAG}" ]]; then
   echo "Image tag already set as ${IMAGE_TAG}"
-else
-  change_image_tag
+  exit 0
 fi
 
-if [[ "${CHANGED}" = true ]]; then
-  git push -u origin "${BRANCH}"
-  gh api repos/alphagov/govuk-helm-charts/merges -f head="${BRANCH}" -f base=main
-  git push origin --delete "${BRANCH}"
+git checkout -b "${BRANCH}"
 
-  echo "Pushed changes to GitHub"
-fi
+yq -i '.image_tag = env(IMAGE_TAG)' "${FILE}"
+yq -i '.promote_deployment = env(PROMOTE_DEPLOYMENT)' "${FILE}"
+
+git add "${FILE}"
+git commit -m "Update ${REPO_NAME} image tag to ${IMAGE_TAG} for ${ENVIRONMENT}"
+
+git push -u origin "${BRANCH}"
+gh api repos/alphagov/govuk-helm-charts/merges -f head="${BRANCH}" -f base=main
+git push origin --delete "${BRANCH}"
+
+echo "Pushed changes to GitHub"
