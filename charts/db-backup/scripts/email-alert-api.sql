dfc643e5c817e36c9a71f7daf13e6e2868acddf6
David Mays <david.mays@digital.cabinet-office.gov.uk>
2025-01-06 18:17:22 +0000

    Fix failing SQLFluff Linti Checks

--- charts/db-backup/scripts/email-alert-api.sql
+++ charts/db-backup/scripts/email-alert-api.sql
@@ -5,14 +5,14 @@
 --
 -- It tries to preserve structure so that multiple occurences of the same
 -- address are mapped to the same "anonymous" address.
-
 -- TODO: generate sufficiently realistic synthetic test data and stop copying
 -- production data outside of production altogether.
-
 -- Drop all emails more than a day old.
-ALTER TABLE emails RENAME TO old_emails;
+ALTER TABLE emails
+RENAME TO old_emails;
 
-CREATE TABLE emails (
+CREATE TABLE
+emails (
   LIKE old_emails INCLUDING CONSTRAINTS INCLUDING DEFAULTS INCLUDING INDEXES
 );
 
@@ -28,25 +28,46 @@ DROP TABLE old_emails CASCADE;
 CREATE TEMP TABLE addresses (id serial, address varchar NOT NULL);
 
 -- Copy all email addresses into the table.
-INSERT INTO addresses (address)
-SELECT address FROM subscribers WHERE address IS NOT NULL;
+INSERT INTO
+addresses (address)
+SELECT address
+FROM
+  subscribers
+WHERE
+  address IS NOT NULL;
 
 CREATE UNIQUE INDEX addresses_index ON addresses (address);
 
-INSERT INTO addresses (address)
-SELECT address FROM emails
+INSERT INTO
+addresses (address)
+SELECT address
+FROM
+  emails
 ON CONFLICT DO NOTHING;
 
 -- Redact address-containing fields in the subscribers and emails tables.
 UPDATE subscribers s
-SET address = concat('anon-', a.id, '@example.com')
-FROM addresses AS a
-WHERE a.address = s.address;
+SET
+  address = concat('anon-', a.id, '@example.com')
+FROM
+  addresses AS a
+WHERE
+  a.address = s.address;
 
 UPDATE emails e
 SET
   address = concat('anon-', a.id, '@example.com'),
-  subject = replace(e.subject, e.address, concat('anon-', a.id, '@example.com')),
-  body = replace(e.body, e.address, concat('anon-', a.id, '@example.com'))
-FROM addresses AS a
-WHERE a.address = e.address;
+  subject = replace(
+    e.subject,
+    e.address,
+    concat('anon-', a.id, '@example.com')
+  ),
+  body = replace(
+    e.body,
+    e.address,
+    concat('anon-', a.id, '@example.com')
+  )
+FROM
+  addresses AS a
+WHERE
+  a.address = e.address;
--- charts/db-backup/scripts/publishing-api.sql
+++ charts/db-backup/scripts/publishing-api.sql
@@ -1,23 +1,34 @@
-DELETE FROM events WHERE created_at < NOW() - INTERVAL '1 month';
+DELETE FROM events
+WHERE
+  created_at < NOW() - INTERVAL '1 month';
 
-UPDATE events SET payload = NULL
+UPDATE events
+SET
+  payload = NULL
 WHERE
   action = 'PutContent'
   AND content_id IN (
     SELECT documents.content_id
-    FROM documents
+    FROM
+      documents
     INNER JOIN editions ON (documents.id = editions.document_id)
     INNER JOIN access_limits ON (editions.id = access_limits.edition_id)
   );
 
-DELETE FROM change_notes WHERE edition_id IN (
-  SELECT edition_id
-  FROM access_limits
-);
+DELETE FROM change_notes
+WHERE
+  edition_id IN (
+    SELECT edition_id
+    FROM
+      access_limits
+  );
 
-DELETE FROM editions WHERE id IN (
-  SELECT edition_id
-  FROM access_limits
-);
+DELETE FROM editions
+WHERE
+  id IN (
+    SELECT edition_id
+    FROM
+      access_limits
+  );
 
 DELETE FROM access_limits;
